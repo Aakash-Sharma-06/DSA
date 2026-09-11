11576c87a64293a0b2953907c91e8f6a33063721
Aakash-Sharma-06 <aakash0958@gmail.com>
2026-09-11 22:49:24 +0530

    Time: 3 ms (24.01%), Space: 14.5 MB (13.24%) - LeetArchive

---
+++ LeetCode/0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> s(nums.begin(), nums.end()); 
+ 
+        if (s.size() < 3) {
+            return *prev(s.end()); 
+        }
+
+        return *prev(s.end(), 3); 
+    }
+};
